44f2bf692499641adc1bad32fbe629fc7138b167
ahmedsiria <83974992+ahmedsiria@users.noreply.github.com>
2021-08-14 04:04:59 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -1837,22 +1837,22 @@ end
 if msg.content_.ID == "MessageChatAddMembers" then 
 if msg.content_.members_[0].id_ == tonumber(bot_id) then 
 print("it is Bot")
-N = (database:get(bot_id.."Name:Bot") or "سوريا")
+N = (database:get(bot_id.."Name:Bot") or "ويجا")
 tdcli_function ({ID = "GetUser",user_id_ = bot_id,},function(arg,data) 
 tdcli_function ({ID = "GetUserProfilePhotos",user_id_ = bot_id,offset_ = 0,limit_ = 1},function(extra,result,success) 
 if result.photos_[0] then
-keyboard = {} 
-keyboard.inline_keyboard = {
-{
-{text =  الـمـطـور , url="http://t.me/"..sudos.UserName},
-},
-{
-{text =  اضف البوت الي مجموعتك ↯ , url = "https://t.me/"..data.username_.."?startgroup=new"},
-},
+ght = sudos.UserName 
+ght = ght:gsub("%@", "") 
+local msg_id = msg.id_/2097152/0.5   
+keyboard = {}  
+keyboard.inline_keyboard = { 
+{ 
+{text = 'الـمـطـور', url="http://t.me/"..ght}, 
+}, 
 }
 local msg_id = msg.id_/2097152/0.5
 local Texti = "٭ مرحبا انا بوت "..N.." \n↞ اختصاصي ادارة المجموعات من السبام والخ..\n↞ للتفعيل ارفعني مشرف وارسل تفعيل في المجموعه ."
-https.request("https://api.telegram.org/bot"..token.. /sendPhoto?chat_id= ..msg.chat_id_.. &caption= ..URL.escape(Texti).. &photo= ..result.photos_[0].sizes_[1].photo_.persistent_id_.. &reply_to_message_id= ..msg_id.. &disable_web_page_preview=true&reply_markup= ..JSON.encode(keyboard)) 
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id='..msg.chat_id_..'&caption='..URL.escape(Texti)..'&photo='..result.photos_[0].sizes_[1].photo_.persistent_id_..'&reply_to_message_id='..msg_id..'&disable_web_page_preview=true&reply_markup='..JSON.encode(keyboard)) 
 end
 end,nil)
 end,nil)
